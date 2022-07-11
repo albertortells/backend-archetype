55103fffd9f3fb4ca9738fbe9970e8ac3fc9d008
Albert Ortells i Cornellas <albertortells@omsis3.es>
2022-07-11 14:08:43 +0200

    Testing update v1
    Trying first update versions for this archetype. Maybe discard.

--- src/main/java/com/brickncode/backendarchetype/controller/ExampleController.java
+++ src/main/java/com/brickncode/backendarchetype/controller/ExampleController.java
@@ -5,6 +5,8 @@ import com.brickncode.backendarchetype.common.URLConstant;
 import com.brickncode.backendarchetype.data.model.input.ExamplePutInput;
 import com.brickncode.backendarchetype.data.model.input.ExamplePostInput;
 import com.brickncode.backendarchetype.service.interfaces.ExampleService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -21,14 +23,19 @@ public class ExampleController {
 	@Autowired
 	private ExampleService service;
 
+	private static final Logger LOGGER = LogManager.getLogger(ExampleController.class);
+
 	@GetMapping(path = URLConstant.GETALL, produces = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody GenericResponse getAllExamples() {
+		LOGGER.info("GetAll endpoint called.");
 		return service.getAllExamples();
 	}
 
 	@GetMapping(path = URLConstant.GETONE + URLConstant.ID_VARIABLE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody GenericResponse getOneExampleByID(@Valid @PathVariable Integer id) {
+		LOGGER.info("GetOne endpoint called.");
 		if(id == null || id < 0) {
+			LOGGER.error("PathVariable id: {} is invalid. Or null o lower than 0.", id);
 			return new GenericResponse(Response.Status.BAD_REQUEST.getStatusCode(), Response.Status.BAD_REQUEST.getReasonPhrase());
 		}
 		return service.getExampleByID(id);
